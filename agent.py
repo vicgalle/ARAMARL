ce64bb008c1c51bf48883db1e2304760ce2dfabb
Victor Gallego <vicgalle@ucm.es>
2018-05-21 17:33:41 +0200

    Better structure, added some agents

--- agent.py
+++ agent.py
@@ -5,35 +5,46 @@ from engine import RMG
 
 
 class Agent():
-    """Parent abstract Agent."""
+    """
+    Parent abstract Agent.
+    """
 
     def __init__(self, action_space):
         self.action_space = action_space
 
     def act(self, obs):
+        """
+        This implementes the policy, \pi(s) -> action.
+        obs is the observed state s
+        """
         raise NotImplementedError()
 
-    def episode_end(self, reward):
-        """This is called at the end of the episode to let the agent know that
-        the episode has ended and what is the reward.
-        Args:
-          reward: The single reward scalar to this agent.
+    def update(self, obs, actions, rewards, new_obs):
+        """
+        This is after an interaction has ocurred, ie all agents have done their respective actions, observed their rewards and arrived at
+        a new observation (state).
+        For example, this is were a Q-learning agent would update her Q-function
         """
         pass
 
 
 class DummyAgent(Agent):
-    """A dummy and stubborn agent that always takes the first action, no matter what happens."""
+    """
+    A dummy and stubborn agent that always takes the first action, no matter what happens.
+    """
 
     def act(self, obs=None):
         # obs is the state (in this case)
-        # This implementes the policy, \pi(s) -> action.
 
         return self.action_space[0]
 
+    " This agent is so simple it doesn't even need to implement the update method! "
+
 
 class RandomAgent(Agent):
-    """An agent that with probability p chooses the first action"""
+    """
+    An agent that with probability p chooses the first action
+    """
 
     def __init__(self, action_space, p):
         Agent.__init__(self, action_space)
@@ -44,7 +55,35 @@ class RandomAgent(Agent):
         assert len(self.action_space) == 2
         return choice(self.action_space, p=[self.p, 1-self.p])
 
+    " This agent is so simple it doesn't even need to implement the update method! "
 
 
+class IndQLearningAgent(Agent):
+    """
+    A Q-learning agent that treats other players as part of the environment (independent Q-learning).
+    She represents Q-values in a tabular fashion, i.e., using a matrix Q.
+    Intended to use as a baseline
+    """
 
+    def __init__(self, action_space, n_states, learning_rate, epsilon, gamma):
+        Agent.__init__(self, action_space)
 
+        self.n_states = n_states
+        self.alpha = learning_rate
+        self.epsilon = epsilon
+        self.gamma = gamma
+        # This is the Q-function Q(s, a)
+        self.Q = np.zeros([self.n_states, len(self.action_space)])
+
+    def act(self, obs=None):
+        """An epsilon-greedy policy"""
+        if np.random.rand() < self.epsilon:
+            return choice(self.action_space)
+        else:
+            return self.action_space[np.argmax(self.Q[obs, :])]
+
+    def update(self, obs, actions, rewards, new_obs):
+        """The vanilla Q-learning update rule"""
+        a0, _ = actions
+        r0, _ = rewards
+        self.Q[obs, a0] = (1 - self.alpha)*self.Q[obs, a0] + self.alpha*(r0 + self.gamma*np.max(self.Q[new_obs, :]))
